b484a949858519106bd1c4c171797da1d0445d37
aikinjess <aikinjess@gmail.com>
2021-03-29 18:52:21 -0400

    comments update

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const commentsCtrl = require('../controllers/comments')
 
 router.get('/new', commentsCtrl.new);
 router.post('/', commentsCtrl.create);
-router.get('/:id/show', commentsCtrl.show)
+router.get('/show', commentsCtrl.show)
 router.delete('/:id', commentsCtrl.deleteComment);
 
 module.exports = router 
